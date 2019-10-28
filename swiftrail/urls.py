73f9210033ac9d71accc7b33ecf132fff556bb01
Ashish Kumar <ashishkr23438@gmail.com>
2019-10-28 23:18:07 +0530

    Added HTML Templates - PNR Status, Train Search, Train Schedule

--- swiftrail/urls.py
+++ swiftrail/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('pnr-status/', views.pnr_status, name='pnr-status'),
+    path('book-ticket/', views.book_ticket, name='book-ticket'),
+    path('train-enquiry/', views.train_enquiry, name='train-enquiry'),
+    path('train-search/', views.train_search, name='train-search'),
+    path('train-schedule/', views.train_schedule, name='train-schedule'),
 ]
--- swiftrail/views.py
+++ swiftrail/views.py
@@ -8,3 +8,18 @@ def login(request):
 
 def register(request):
     return render(request, 'register.html')
+
+def pnr_status(request):
+    return render(request, 'pnr-status.html')
+
+def train_enquiry(request):
+    return render(request, 'train-enquiry.html')
+
+def train_schedule(request):
+    return render(request, 'train-schedule.html')
+
+def train_search(request):
+    return render(request, 'train-search.html')
+
+def book_ticket(request):
+    return render(request, 'book-ticket.html')
